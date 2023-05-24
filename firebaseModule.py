90608183d866bb9451585443e4a71564abfdc252
lordkemono <37463322+JumixTuna@users.noreply.github.com>
2023-05-23 23:26:16 -0600

    Delete firebaseModule.py

--- firebaseModule.py
+++
@@ -1,7 +0,0 @@
-def agregar(db, coll, dictData, doc = False):
-    if not doc:
-        doc_ref = db.collection(coll).document()
-        doc_ref.set(dictData)
-    else:
-        doc_ref = db.collection(coll).document(doc)
-        doc_ref.set(dictData)
